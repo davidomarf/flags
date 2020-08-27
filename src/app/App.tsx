e5ec3662bb0d2e4d880105c71f4711953da3762c
David Omar Flores Chávez <david@davidomar.com>
2020-08-27 16:20:52 -0500

    Move App to its own directory to hold all its children components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import classnames from "classnames/bind";
 
 import styles from "./App.module.scss";
+import Header from "./Header/Header";
 
 const cx = classnames.bind(styles);
 
-function App() {
-  return <div className={cx("App")}>Hey</div>;
-}
+const App = () => (
+  <div className={cx("App")}>
+    <Header />
+  </div>
+);
 
 export default App;
