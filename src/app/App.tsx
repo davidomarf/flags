8b4ea7b9fbeb47d6600f79e4ed063ed85110296b
David Omar Flores Chávez <david@davidomar.com>
2020-08-27 16:51:14 -0500

    Create mock countries

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,12 +6,32 @@ import Filter from "./Filter/Filter";
 
 import styles from "./App.module.scss";
 
+const countries = [
+  {
+    name: "Germany",
+    population: 81770900,
+    region: "Europe",
+    capital: "Berlin"
+  },
+  {
+    name: "Brazil",
+    population: 201770900,
+    region: "Americas",
+    capital: "Brasilia"
+  }
+];
+
 const App = () => (
   <div className={styles.App}>
     <Header />
     <div className={styles.mainContent}>
-      <SearchBar />
-      <Filter />
+      <div className={styles.row}>
+        <SearchBar />
+        <Filter />
+      </div>
+      <div className={styles.row}>
+        <Countries countries={countries.concat(countries.concat(countries))} />
+      </div>
     </div>
   </div>
 );
