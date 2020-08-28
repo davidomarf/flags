6ddb0e426dd2b0ea60e101c3796fe4f3ee4fa139
David Omar Flores Chávez <david@davidomar.com>
2020-08-27 20:08:34 -0500

    Use the Lens Icon

--- src/app/SearchBar/SearchBar.tsx
+++ src/app/SearchBar/SearchBar.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 
+import { ReactComponent as Lens } from "../../assets/icons/lens.svg";
+
 import style from "./SearchBar.module.scss";
 
 const SearchBar = () => (
   <div className={style.searchBar}>
     <div className={style.field}>
-      <span>O</span>
+      <span>
+        <Lens />
+      </span>
       <input placeholder="Search for a country..."></input>
     </div>
   </div>
