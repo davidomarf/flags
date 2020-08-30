63fc477ccd506a12d51771d3326feda47a16de27
David Omar Flores Chávez <david@davidomar.com>
2020-08-29 22:10:24 -0500

    Replace all the logic to set a focused country

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,4 @@
-import React, {
-  useState,
-  useCallback,
-  useRef,
-  RefObject,
-  useEffect
-} from "react";
+import React, { useState, useCallback } from "react";
 import {
   BrowserRouter as Router,
   Switch,
@@ -24,17 +18,8 @@ import CountryDetails from "./CountryDetails/CountryDetails";
 
 import styles from "./App.module.scss";
 
-const scrollToRef = (ref: RefObject<HTMLDivElement>) =>
-  ref?.current &&
-  window.scrollTo({ top: ref.current.offsetTop - 100, behavior: "smooth" });
-
 const App = () => {
-  const executeScroll = () => scrollToRef(focusedCountryRef);
-
-  const focusedCountryRef = useRef<HTMLDivElement>(null);
-
   const [query, setQuery] = useState<RegExp>(new RegExp("."));
-  const [focusedCountry, setFocusedCountry] = useState<Country>();
 
   const { data, isLoading, error } = useQuery<Country[]>(
     "Countries",
@@ -51,17 +36,6 @@ const App = () => {
     [setQuery]
   );
 
-  const focusCountry = useCallback(
-    (country: Country) => {
-      setFocusedCountry(country);
-    },
-    [setFocusedCountry]
-  );
-
-  useEffect(() => {
-    executeScroll();
-  }, [focusedCountry]);
-
   return (
     <Router>
       <div className={styles.App}>
@@ -76,24 +50,18 @@ const App = () => {
                 <SearchBar searchFor={searchFor} />
                 <Filter />
               </div>
-              {focusedCountry && (
-                <div className={styles.row} ref={focusedCountryRef}>
-                  <CountryDetails country={focusedCountry} />
+
+              {isLoading ? (
+                <>Loading...</>
+              ) : error ? (
+                <>Error. :(</>
+              ) : data ? (
+                <div className={styles.row}>
+                  <Countries
+                    countries={data.filter((e) => e.name?.match(query))}
+                  />
                 </div>
-              )}
-              {!focusedCountry &&
-                (isLoading ? (
-                  <>Loading...</>
-                ) : error ? (
-                  <>Error. :(</>
-                ) : data ? (
-                  <div className={styles.row}>
-                    <Countries
-                      countries={data.filter((e) => e.name?.match(query))}
-                      focusCountry={focusCountry}
-                    />
-                  </div>
-                ) : null)}
+              ) : null}
             </div>
           </Route>
           <Redirect to="/" />
